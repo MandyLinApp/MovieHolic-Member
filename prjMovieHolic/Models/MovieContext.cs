c87aca3c73d45dc64e4cd61a627d7e292ddd878e
MandyLin <mandyeee12@gmail.com>
2023-05-26 10:10:16 +0800

    5/26 連線字串變更

--- prjMovieHolic/Models/MovieContext.cs
+++ prjMovieHolic/Models/MovieContext.cs
@@ -117,7 +117,7 @@ public partial class MovieContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=NeilLiuTW.asuscomm.com, 1433; Initial Catalog=Movie; User ID=movieTeam; Password=m2023Team; TrustServerCertificate=true");
+        => optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=Movie;Integrated Security=True;TrustServerCertificate=true");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
